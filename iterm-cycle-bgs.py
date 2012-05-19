defb8778da380f324cd477c33fc5f9eca18c8f04
katy lavallee <katylava@gmail.com>
2012-05-18 23:17:53 -0500

    iterm-cycle-bgs.py: refactored to fix several bugs

--- iterm-cycle-bgs.py
+++ iterm-cycle-bgs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from appscript import app, its
-from splitscreenbgs import get_size_from_image, choose_pix, make_bg, GenerateImageError
+import splitscreenbgs as ss
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 800
@@ -32,19 +32,26 @@ class ItermSessionBG:
             else:
                 self.set_session_bg() # sets self.current
 
-        self.current_prefix = self.current.split('/')[-1].rpartition('.')[0]
-        self.prefix = prefix or self.current_prefix
+        if not self.prefix:
+            self.prefix = self.current.split('/')[-1].split('.')[0]
 
     def filename(self):
         return '{}.{}'.format(self.prefix, self.num)
 
-    def set_session_bg(self):
-        self.current = '{}/{}.jpg'.format(OUTPUT_DIR, self.filename())
-        self.session.background_image_path.set(self.current)
-        return self.current
+    def filepath(self):
+        return '{}/{}.jpg'.format(OUTPUT_DIR, self.filename())
+
+    def set_session_bg(self, path=None):
+        if not path:
+            path = self.filepath()
+        self.session.background_image_path.set(path)
+        return path
+
+    def unset_bg(self):
+        self.session.background_image_path.set(u'')
 
     def get_ratio(self):
-        size = get_size_from_image(self.prefix, OUTPUT_DIR)
+        size = ss.get_size_from_image(self.prefix, OUTPUT_DIR)
         return [(
             self.prefix,
             float(size[0])/SCREEN_WIDTH,
@@ -53,37 +60,53 @@ class ItermSessionBG:
 
     def change_session_bg(self):
         ratio = self.get_ratio()
-        images = choose_pix(INPUT_DIR, SCREEN_WIDTH, SCREEN_HEIGHT, ratio)
+        images = ss.choose_pix(INPUT_DIR, SCREEN_WIDTH, SCREEN_HEIGHT, ratio)
         spec = images[self.prefix]
 
         try:
-            im = make_bg(spec['file'], spec['size'])
-        except GenerateImageError as e:
+            im = ss.make_bg(spec['file'], spec['size'])
+        except ss.GenerateImageError as e:
             print("\tError generating image: {}".format(e.message))
         else:
             try:
-                im.save(self.current)
+                im.save(self.filepath())
             except Exception as e:
-                print("\tError saving image to {}: {}".format(self.current,
+                print("\tError saving image to {}: {}".format(self.filepath(),
                                                               e.message))
+            else:
+                return self.set_session_bg(self.filepath())
+
 
 
 if __name__ == '__main__':
     import os, sys
+    from optparse import OptionParser
+    usage = "Usage: %prog [options]"
+    parser = OptionParser(usage=usage)
+    parser.add_option('-p', '--prefix')
+    parser.add_option('-u', '--unset', action='store_true')
+    parser.add_option('-l', '--list', action='store_true',
+                      help='List current prefixes in out_dir')
+    options, args = parser.parse_args()
+
+    if options.list:
+        ss.list_sizes(OUTPUT_DIR)
+        exit()
 
     fd = sys.stdout.fileno()
     tty = os.ttyname(fd)
-    argc = len(sys.argv)
-    prefix = None if argc<2 else sys.argv[1]
+    if not tty:
+        print('Error getting tty')
+        exit()
 
-    bg = ItermSessionBG(tty, prefix)
-    bg.change_session_bg()
+    bg = ItermSessionBG(tty, options.prefix)
+    if options.unset:
+        bg.unset_bg()
+    else:
+        print bg.change_session_bg()
 
-    # refresh session by resizing text
+    # refresh screen by resizing text
     iterm = app('System Events').processes['iTerm']
     menu = iterm.menu_bars[0].menus['View'].menu_items
     menu['Make Text Smaller'].click()
     menu['Make Text Bigger'].click()
-
-    print(bg.current)
-
